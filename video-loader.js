2b38a98f79d5b480da983fceeee11120982c4e6a
HelloXiuXiu <xiuxiuweb@gmail.com>
2026-02-23 21:57:19 +0100

    test

--- video-loader.js
+++ video-loader.js
@@ -1,15 +1,15 @@
-"use strict"
+// "use strict"
 
-function loadVideos() {
-  document.querySelectorAll(".project-video").forEach(video => {
-    if (video.dataset.loaded) return
-    video.dataset.loaded = "true"
-    video.src = video.dataset.src
-  })
-};
+// function loadVideos() {
+//   document.querySelectorAll(".project-video").forEach(video => {
+//     if (video.dataset.loaded) return
+//     video.dataset.loaded = "true"
+//     video.src = video.dataset.src
+//   })
+// };
 
-["mouseenter", "touchstart"].forEach(event =>
-  document
-    .querySelector(".main-navigation")
-    .addEventListener(event, loadVideos, { once: true })
-)
+// ["mouseenter", "touchstart"].forEach(event =>
+//   document
+//     .querySelector(".main-navigation")
+//     .addEventListener(event, loadVideos, { once: true })
+// )
